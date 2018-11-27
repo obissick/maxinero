3bea8ddab4ebf76b5bccd295bc471a9bf9cf84a9
obissick <obissick@greathealthworks.com>
2018-11-27 01:49:01 -0500

    redir on register.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-use URL;
-
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -15,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        
+
     }
 
     /**
